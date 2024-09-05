2f0bca4735de5cdef42742e17f91bf1879d18a69
vatsamit <128714694+vatsamit@users.noreply.github.com>
2024-09-05 14:04:00 +0530

    app deployed

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv" ;
 import mongoose from 'mongoose';
 import cors from 'cors' ;
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -33,6 +34,17 @@ try {
 app.use("/api/user" ,userRoute);
 app.use("/api/message" ,messageRoute);
 
+//-------------------code for deployment-----------//
+if(process.env.NODE_ENV==="production"){
+  const dirPath = path.resolve();
+
+  app.use(express.static("./Frontend/dist"));
+  app.get("*" , (req,res) => {
+    res.sendFile(path.resolve(dirPath,"./Frontend/dist" ,"index.html"))
+  } )
+}
+
+//server
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
